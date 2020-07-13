865f0ae6d7be655a4a7c6d7b00183bd8b232ed0c
Nilolar-Aleksandrovich <hyyscience@163.com>
2020-07-13 23:01:38 +0800

    7.13practice

---
+++ 链表删除操作.java
@@ -0,0 +1,19 @@
+package com.pratiace.fristpackage;
+//请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+
+    }
+}
+//从链表里删除一个节点 node 的最常见方法是修改之前节点的 next 指针，使其指向之后的节点。
