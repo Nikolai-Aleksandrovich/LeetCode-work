bb67b36a18d0c38d326cd7f955184c5835dd887a
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2020-09-08 17:37:45 +0800

    Create 数组拆分令最小值和最大.java

---
+++ 数组拆分令最小值和最大.java
@@ -0,0 +1,15 @@
+package com.itranswarp.sample;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+        //之理解了一种基于排序的想法，而还有两种，分别是暴力使用permute(nums,indexs)和使用额外空间的想法
+        /**int sum = 0;
+        Arrays.sort(nums);
+        for(int i = 0;i< nums.length;i+=2){
+            sum=nums[i];
+        }
+        return sum;*/
+    }
+}
