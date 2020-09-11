8a5464bee8dae289a845e1cb602b4b5592064724
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2020-09-11 23:16:05 +0800

    Update cosine similarity.py

--- HYYcode/cosine similarity.py
+++ HYYcode/cosine similarity.py
@@ -1,8 +1,9 @@
 import time
-
+import networkx as nx
 
 import sklearn.metrics.pairwise as pw
 import numpy as np
+
 
 def cos_sim(vector_a, vector_b):
     """
@@ -11,6 +12,7 @@ def cos_sim(vector_a, vector_b):
     :param vector_b: 向量 b
     :return: sim
     """
+
     vector_a = np.mat(vector_a)
     vector_b = np.mat(vector_b)
     num = float(vector_a * vector_b.T)
@@ -49,12 +51,28 @@ def cosine_similarity(x, y, norm=False):
     return 0.5 * cos + 0.5 if norm else cos  # 归一化到[0, 1]区间内
 
 
+def getCosineSimilarityFromOut(filePath1, filePath2, filePath3, k):
+    # start_time = time.time()
 
+    try:
+        f = open(filePath3)
+    except IOError:
+        print("---**---打开文件失败！请检查是否存在该文件！并重新输入文件名!---**---")
+    print("filePath:", filePath3)
+    TestVectorsCount = 0
+    EdgelistNodesCount = 0
+    EdgeListNodeList = []
+    for line in f.readlines():
+        EdgelistNodesCount = EdgelistNodesCount + 1
 
-def getEdgesFromCSVtoGraph(filePath1,filePath2, lontitudelatitudeArray, mytree, IfPrint):
+        lines = (line.strip().split(" "))
+        TestVectorsCount = TestVectorsCount + 1
+        EdgeListNodeList.append(lines)
 
-    start_time = time.time()
+    EdgeListNode = np.array(EdgeListNodeList, dtype=float)
+    # print(EdgeListNode)
 
+    # 20%测试样本
     try:
         f = open(filePath1)
     except IOError:
@@ -64,17 +82,15 @@ def getEdgesFromCSVtoGraph(filePath1,filePath2, lontitudelatitudeArray, mytree, 
     TestitemsCount = 0
     TestVectorList = []
     for line in f.readlines():
-
         TestitemsCount = TestitemsCount + 1
-        if TestitemsCount == 1:
-            continue
-        else:
-            lines = line.strip().split(",")
-            TestVectorsCount = TestVectorsCount + 1
-            TestVectorList.append(lines)
 
-    TestVectorsArray = np.array(TestVectorList)
+        lines = line.strip().split(",")
+        TestVectorsCount = TestVectorsCount + 1
+        TestVectorList.append(lines)
+    TestVectorsArray = np.array(TestVectorList, dtype=float)
+    # print(TestVectorsArray)
 
+    # 80%原样本
     try:
         f = open(filePath2)
     except IOError:
@@ -84,19 +100,14 @@ def getEdgesFromCSVtoGraph(filePath1,filePath2, lontitudelatitudeArray, mytree, 
     TrainitemsCount = 0
     TrainVectorList = []
     for line in f.readlines():
-
         TrainitemsCount = TrainitemsCount + 1
-        if TrainitemsCount == 1:
-            continue
-        else:
-            lines = line.strip().split(",")
-            TrainVectorsCount = TrainVectorsCount + 1
-            TrainVectorList.append(lines)
 
-    TrainVectorsArray = np.array(TrainVectorList)
+        lines = line.strip().split(",")
+        TrainVectorsCount = TrainVectorsCount + 1
+        TrainVectorList.append(lines)
 
-    # 对其按照第1列排序
-    #SortedEdgesArray = edgesArray[edgesArray[:, 0].argsort()]
+    TrainVectorsArray = np.array(TrainVectorList, dtype=float)
+    # SortedTrainVectorsArray = TrainVectorsArray[TrainVectorsArray[:, 0].argsort()]# 对其按照第1列排序
     # 打印排好序的数组
     # print(SortedEdgesArray)
     # #打印原数组
@@ -104,114 +115,87 @@ def getEdgesFromCSVtoGraph(filePath1,filePath2, lontitudelatitudeArray, mytree, 
 
     currentNodeID = ""
     currentCosineSimilarityList = []
+    testNodeID = []  # 小数据集节点ID
+    trainNodeID = []  # 较大训练数据集节点ID
+    testCosineSimilarityList = []
+    allCosineSimilarityList = []
+    finalCSList = [[] for i in range(TestitemsCount)]
+    # finalCSList =[TestitemsCount] [TrainitemsCount]#最终的余弦相似性二维数组
+    a = -1
 
     SortedCarList = []
     ValidCount = 0
-    for i in TestVectorsArray:
-        currentCosineSimilarityList.append(i[0])
-        for k in range(1,128):
-            currentVector = [i[k]]
-        for j in TrainVectorsArray:
-            for g in TrainVectorsArray:
-                dic = {g[0],}
 
-            currentCosineSimilarityList.append(j[0])
-            currentCosineSimilarityList.append(cos_sim())
-
-                    
+    for trainArray in TrainVectorsArray:
+        finaltrainVectorFloat = []
+        trainNodeID.append(trainArray[0])  # 当前节点顺序写入较大数据训练集节点节点序列
 
 
-                    # 其余Weight计算参数
-                    x = float(second[9])  # 下一条旅行记录的收入
-                    d = haversine(float(first[12]), float(first[13]), float(second[10]),
-                                  float(second[11]))  # 一条旅行记录的路程+空车寻客路程
-                    if d == 0:
-                        d = 1
+    for testArray in TestVectorsArray:
+        a = a + 1
+        CSListForEachRow = []
+        finalTestVectorFloat = []
+        testNodeID.append(testArray[0])
+        testVector = np.delete(testArray, 0)
+        for trainArray in TrainVectorsArray:
+            trainVector = np.delete(trainArray, 0)
+            CSListForEachRow.append(cos_sim(testVector, trainVector))# 每行的余弦相似性加入列表
 
-                    n = 1  # 该时间片内从下一条起点出发的旅途数量
+        finalCSList[a] = CSListForEachRow
+    # print('start print finalCSList')
+    # print(finalCSList)
+    # print('end of finalCSList')
+    TopKIndex = [[] for i in range(TestitemsCount)]
+    for i in range(0, TestitemsCount):
+        temp = finalCSList[i]
+        for j in range(0, k):
+            TopKIndex[i].append(temp.index(max(temp)))
+            temp[temp.index(max(temp))] = 0.001
+    # print('start print TopKIndex')
+    # print(TopKIndex)
+    # print('end of TopKIndex')
+    hitratio = []
+    precision = []
+    F1Score = []
+    HitRatioNumber = 0
+    for i in range(0, TestitemsCount):  # 遍历每一个TestID 数据
+        RealDestID = []  # 每一个TestID对应的真实地点ID列表
+        trueNumber = 0  # 表示对第i行存在多少真实地点值的去
+        RecommendDestID = []
+        for j in range(k):  # test[1，2，3，...，k-1,k]遍历每一个test
+            tempIndex = TopKIndex[i][j]
+            RecommendDestID.append(trainNodeID[TopKIndex[i][j]])  # 推荐系统认为对第i行值得去的K个地点
 
-                    # 空车和接到客人的位置信息
-                    EmptyAndpickUpPosition = [[float(first[12]), float(first[13])],
-                                              [float(second[10]), float(second[11])]]
-                    EmptyAndpickUpPositionArray = np.array(EmptyAndpickUpPosition)
+        for line in EdgeListNode:
+            trueDestNumber = 0
+            if (testNodeID[i] == line[0]):
+                RealDestID.append(line[1])
+        ifcomparefinish = False
+        for recomID in RecommendDestID:
+            for realID in RealDestID:
+                if recomID==realID:
+                    HitRatioNumber=HitRatioNumber+1
+                    ifcomparefinish=True
+                    break
+                else:
+                    ifcomparefinish=False
+        if ifcomparefinish==True:
+            continue
+    ans = HitRatioNumber/TestitemsCount
 
-                    # 在KDTree中查找空车和接到客人的位置最近的POI，返回最近距离和POI的ID
-                    distance, index = queryPoint(mytree, EmptyAndpickUpPositionArray)
-                    # print distance, index
+    return ans
+fpath1 = "./data/CosineSimilarityData/realtestdata/resulttest.csv"
+fpath2 = "./data/CosineSimilarityData/vec_all.csv"
+fpath3 = "./data/test1.weighted.edgelist"
 
-                    # TimeNode=0
-                    # 从前一条轨迹中得到信息增益
-                    # print int(index[1])
-                    # print G.nodes[int(index[1])]
-                    InformationEntropy = G.nodes[int(index[1])]["InformationEntropy"]
-                    if IfPrint == 1:
-                        print("InformationEntropy:" + str(InformationEntropy))
-
-                    # 可调参数
-                    alpha = 1
-                    beta = 1
-                    current_total_x = 0
-                    current_total_d = 0
-                    current_total_trip = 0
-
-                    if G.has_edge(index[0], index[1]):
-                        # 存在边就对边的值进行权重加和
-                        # current_total_x = nx.get_edge_attributes(G, 'current_total_x') + x
-                        # current_total_trip = nx.get_edge_attributes(G,'current_total_trip') + n
-
-                        # G.edges[index[0], index[1]]['current_total_d'] = G.edges[index[0], index[1]][
-                        # 'current_total_d'] + d4
-                        # G.edges[index[0], index[1]]['weightOfEdge'] = ( x * InformationEntropy * (
-                        #                                                           alpha * x /
-                        #                                                           n) / (
-                        #                                                                   d * beta))
-                        G.add_edge(index[0], index[1], weight=G.edges[index[0], index[1]]['weight'] + (
-                                    x * InformationEntropy * (
-                                    alpha * x /
-                                    n) / (
-                                            d * beta)))
-
-                        # G.edges[index[0], index[1]]['current_total_x'] = G.edges[index[0], index[1]][
-                        #                                                         'current_total_trip'] + x
-                        # G.edges[index[0], index[1]]['current_total_trip'] = G.edges[index[0], index[1]]['current_total_trip'] + n
-                        # G.edges[index[0], index[1]] ['weightOfEdge'] = (
-                        #         G.edges[index[0], index[1]]['current_total_trip'] * InformationEntropy * (
-                        #         alpha * G.edges[index[0], index[1]]['current_total_x'] /
-                        #         G.edges[index[0], index[1]]['current_total_trip']) / (
-                        #                 G.edges[index[0], index[1]]['current_total_d'] * beta))
-                    else:
-                        # 不存在边就添加边
-                        G.add_edge(index[0], index[1],
-                                   weight=(n * InformationEntropy * alpha * x / n) / (d * beta))
-                    if G.edges[index[0], index[1]]['weight'] == 0.0:
-                        G.edges[index[0], index[1]]['weight'] = 0.000000001
-                        # G.add_edge(index[0], index[1], current_total_x=x, current_total_d=d, current_total_trip=n,
-                        #            weightOfEdge=(n * InformationEntropy * alpha * x / n) / (d * beta))
-            currentCarID = i[0]  # SortedCarList.append(SortedcurrentCarArray))#一个新的CarID)
-            currentCarList = [i]
-
-    # 对最后一个array进行排序
-    if len(currentCarList) != 0 and len(currentCarList) != 1:
-        # 如果车辆的行驶轨迹只有一条数据，那么这一条算无效数据
-        ValidCount = ValidCount + len(currentCarList)
-        currentCarArray = np.array(currentCarList)
-        # print currentCarArray
-        # 对其按照第6列排序
-        SortedcurrentCarArray = currentCarArray[currentCarArray[:, 5].argsort()]
-        if IfPrint == 1:
-            print("--------" + str(currentCarID) + "-----------------")
-            print(SortedcurrentCarArray)
-
-        # SortedCarList.append(SortedcurrentCarArray)
-    # SortedCarArray=np.array(SortedCarList)
-    # print SortedCarArray
-
-    end_time = time.time()
-    cost_time = (end_time - start_time)
-
-    print("The number of edges:" + str(EdgesCount))
-    print("The number of valid edges:" + str(ValidCount))
-    print('Total time spent on loading car data {:.5f} second.'.format(cost_time))
-    print("--------Done!--------")
-    return G
+Hit = getCosineSimilarityFromOut(fpath1, fpath2, fpath3, 10)
+# file1=open("E:/Anaconda/envs/Python35/src/code/essaycode/HYYcode/data/Percision.txt","w")
+# file1.write(str(Perc))
+# file1.close()
+# file2=open("E:/Anaconda/envs/Python35/src/code/essaycode/HYYcode/data/recall.txt","w")
+# file2.write(str(reca))
+# file2.close()
+file3=open("E:/Anaconda/envs/Python35/src/code/essaycode/HYYcode/data/FHit.txt","w")
+file3.write(str(Hit))
+file3.close()
 
