74bf8cdfee1e63d3f4c7fe54d34cbc97087a4b41
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-03-16 21:38:32 +0800

    formalized

--- dailyExercise/no1SumOfTwoNumber/Solution.java
+++ dailyExercise/no1SumOfTwoNumber/Solution.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
  * @author Yuyuan Huang
  * @create 2021-03-16 20:14
  */
-public class Solution {
+class Solution {
     public int[] twoSum(int[] nums, int target){
         HashMap<Integer,Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
--- dailyExercise/no2aAddTwoNumbers/Solution.java
+++ dailyExercise/no2aAddTwoNumbers/Solution.java
@@ -5,4 +5,7 @@ package dailyExercise.no2aAddTwoNumbers;
  * @create 2021-03-16 21:22
  */
 public class Solution {
+    public ListNode addTwoNumbers(ListNode l1,ListNode l2){
+
+    }
 }
