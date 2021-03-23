6f0433a6aa2dc0e60eec392d00cf170e1ed37fd2
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-03-23 23:05:18 +0800

    solution of divideTwoIntegers

---
+++ dailyExercise/no29divideTwoIntegers/Solution.java
@@ -0,0 +1,11 @@
+package no29divideTwoIntegers;
+
+/**
+ * @author Yuyuan Huang
+ * @create 2021-03-23 18:03
+ */
+public class Solution {
+    public int divide(int dividend, int divisor) {
+
+    }
+}
