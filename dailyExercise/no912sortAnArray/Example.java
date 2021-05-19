c138930396767c235f97bb07b11bfe7114e21063
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-05-19 23:48:55 +0800

    update

--- dailyExercise/no912sortAnArray/Example.java
+++ dailyExercise/no912sortAnArray/Example.java
@@ -33,6 +33,6 @@ public class Example {
     }
 
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ dailyExercise/no912sortAnArray/SelectSort.java
@@ -0,0 +1,23 @@
+package no912sortAnArray;
+
+/**
+ * @author Yuyuan Huang
+ * @create 2021-05-19 23:45
+ */
+public class SelectSort {
+    public int[] selectSort(int[] nums){
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if (nums[j]<nums[min]){
+
+                }
+            }
+        }
+    }
+    private static  void  swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i]=num
+    }
+}
